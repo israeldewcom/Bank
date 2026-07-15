18bb73fd7e16ff82fb00b08a11a5285fee8c79b3
israeldewcom <haddockthor@gmail.com>
2026-07-15 22:04:32 +0100

    Create middleware.py

---
+++ chronos_v5/api/middleware.py
@@ -0,0 +1,11 @@
+from starlette.middleware.base import BaseHTTPMiddleware
+import uuid
+from chronos_v5.logger_setup import logger
+
+class CorrelationIdMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request, call_next):
+        correlation_id = request.headers.get("X-Correlation-ID", str(uuid.uuid4()))
+        request.state.correlation_id = correlation_id
+        response = await call_next(request)
+        response.headers["X-Correlation-ID"] = correlation_id
+        return response
