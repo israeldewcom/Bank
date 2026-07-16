e907494e746125681a1412e21241824c95c3fb81
israeldewcom <haddockthor@gmail.com>
2026-07-16 15:06:17 +0100

    Update init.py

--- chronos_v5/api/init.py
+++ chronos_v5/api/init.py
@@ -1,1 +1,16 @@
 # api package
+# chronos_v5/api/routers/__init__.py
+
+from . import admin
+from . import audit
+from . import backtest
+from . import collateral
+from . import dashboard
+from . import execution
+from . import model
+from . import nibss
+from . import pricing
+from . import risk
+from . import system_health
+from . import trade
+from . import websocket
